e7fdbdaf3e8c5a2528f5690bd3c3c2ebba617821
Christina Sun <che.sun.1996@gmail.com>
2025-05-06 18:25:22 -0700

    transcribe new meeting

--- transcription/transcribe.py
+++ transcription/transcribe.py
@@ -4,8 +4,8 @@ import whisper
 model = whisper.load_model("turbo")
 # result = model.transcribe("test.m4a")
 
-result = model.transcribe("lunch_talk.m4a")
+result = model.transcribe("anujit_may_6.m4a")
 # print(result["text"])
 
-with open("lunch_talk_transcription.txt", "w") as file:
+with open("anujit_may_6_transcription.txt", "w") as file:
     file.write(result["text"])
